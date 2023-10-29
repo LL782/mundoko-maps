99cbd276883f0c231e01e97f92ad52e6e0cb6032
Laurence Lord <laurence.lord@ledger.fr>
2023-10-29 13:22:55 +0000

    Add ts-jest

--- Mundoko-Maps/jest.config.ts
+++ Mundoko-Maps/jest.config.ts
@@ -24,7 +24,7 @@ const config: Config = {
   // moduleNameMapper: {},
   // modulePathIgnorePatterns: [],
   // notify: false,
-  // preset: undefined,
+  preset: "ts-jest",
   // resetMocks: false,
   // resetModules: false,
   // resolver: undefined,
