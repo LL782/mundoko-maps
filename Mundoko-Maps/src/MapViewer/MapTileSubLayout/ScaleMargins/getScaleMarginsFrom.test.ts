fb4537515c462ce07e3a1b46c5a377dd47331053
Laurence Lord <laurence.lord@ledger.fr>
2023-10-29 16:24:51 +0000

    Expect four digit strings

--- Mundoko-Maps/src/MapViewer/MapTileSubLayout/ScaleMargins/getScaleMarginsFrom.test.ts
+++ Mundoko-Maps/src/MapViewer/MapTileSubLayout/ScaleMargins/getScaleMarginsFrom.test.ts
@@ -3,8 +3,8 @@ import { getScaleMarginsFrom } from "./getScaleMarginsFrom";
 
 let testPosition: PagePosition = {} as any;
 let result: ReturnType<typeof getScaleMarginsFrom>;
-let northSouthPoints: number[];
-let westEastPoints: number[];
+let northSouthPoints: string[];
+let westEastPoints: string[];
 
 describe("given placeholder page position", () => {
   beforeEach(() => {
@@ -24,11 +24,16 @@ describe("given placeholder page position", () => {
     expect(westEastPoints.length).toBe(11);
   });
 
-  test("input East and South return as central points", () => {
+  test("points are four digit strings", () => {
+    northSouthPoints.forEach((p) => expect(p.length).toBe(4));
+    westEastPoints.forEach((p) => expect(p.length).toBe(4));
+  });
+
+  test("input East and South are the central points", () => {
     const middleIndex1 = Math.round(northSouthPoints.length / 2) - 1;
     const middleIndex2 = Math.round(westEastPoints.length / 2) - 1;
 
-    expect(northSouthPoints[middleIndex1]).toBe(testPosition.south);
-    expect(westEastPoints[middleIndex2]).toBe(testPosition.east);
+    expect(northSouthPoints[middleIndex1]).toBe("5155");
+    expect(westEastPoints[middleIndex2]).toBe("5135");
   });
 });
