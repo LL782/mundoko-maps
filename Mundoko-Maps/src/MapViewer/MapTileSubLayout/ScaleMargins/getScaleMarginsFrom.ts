5784f894e6f1fff7c247d7050325aad98959f5ae
Laurence Lord <laurence.lord@ledger.fr>
2023-10-29 15:52:50 +0000

    Use values from helper to drive UI

--- Mundoko-Maps/src/MapViewer/MapTileSubLayout/ScaleMargins/getScaleMarginsFrom.ts
+++ Mundoko-Maps/src/MapViewer/MapTileSubLayout/ScaleMargins/getScaleMarginsFrom.ts
@@ -2,7 +2,15 @@ import { PagePosition } from "../../../types/PagePosition";
 
 export const getScaleMarginsFrom = (pagePosition: PagePosition) => {
   return {
-    northSouth: { points: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] },
-    westEast: { points: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] },
+    northSouth: {
+      points: [
+        5150, 5151, 5152, 5153, 5154, 5155, 5156, 5157, 5158, 5159, 5160,
+      ],
+    },
+    westEast: {
+      points: [
+        5130, 5131, 5132, 5133, 5134, 5135, 5136, 5137, 5138, 5139, 5140,
+      ],
+    },
   };
 };
--- Mundoko-Maps/src/MapViewer/MapTileSubLayout/ScaleMargins/index.tsx
+++ Mundoko-Maps/src/MapViewer/MapTileSubLayout/ScaleMargins/index.tsx
@@ -12,37 +12,20 @@ export const ScaleMargins = ({ position }: Props) => {
     westEast: { points: westEastPoints },
   } = getScaleMarginsFrom(position);
 
-  console.log("northSouthPoints:", northSouthPoints);
-  console.log("westEastPoints:", westEastPoints);
-
   return (
     <>
       <ol className={`${style.northSouthMargin} ${style.margin}`}>
-        <li className={style.marker}>5150</li>
-        <li className={style.marker}>5151</li>
-        <li className={style.marker}>5152</li>
-        <li className={style.marker}>5153</li>
-        <li className={style.marker}>5154</li>
-        <li className={style.marker}>5155</li>
-        <li className={style.marker}>5156</li>
-        <li className={style.marker}>5157</li>
-        <li className={style.marker}>5158</li>
-        <li className={style.marker}>5159</li>
-        <li className={style.marker}>5160</li>
+        {northSouthPoints.map(toMarginMarker)}
       </ol>
       <ol className={`${style.westEastMargin} ${style.margin}`}>
-        <li className={style.marker}>5130</li>
-        <li className={style.marker}>5131</li>
-        <li className={style.marker}>5132</li>
-        <li className={style.marker}>5133</li>
-        <li className={style.marker}>5134</li>
-        <li className={style.marker}>5135</li>
-        <li className={style.marker}>5136</li>
-        <li className={style.marker}>5137</li>
-        <li className={style.marker}>5138</li>
-        <li className={style.marker}>5139</li>
-        <li className={style.marker}>5140</li>
+        {westEastPoints.map(toMarginMarker)}
       </ol>
     </>
   );
 };
+
+const toMarginMarker = (n: number) => (
+  <li key={n} className={style.marker}>
+    {n}
+  </li>
+);
