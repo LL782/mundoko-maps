4891faf7263b67f61ba4b7861817c59bf177721f
Laurence Lord <laurence.lord@ledger.fr>
2023-10-29 13:16:19 +0000

    Add first Jest test

---
+++ Mundoko-Maps/jest.config.ts
@@ -0,0 +1,59 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  // automock: true,
+  bail: 1,
+  clearMocks: true,
+  // globalSetup: undefined,
+  // globalTeardown: undefined,
+  // globals: {},
+  // maxWorkers: "50%",
+  // moduleDirectories: [
+  //   "node_modules"
+  // ],
+  // moduleFileExtensions: [
+  //   "js",
+  //   "mjs",
+  //   "cjs",
+  //   "jsx",
+  //   "ts",
+  //   "tsx",
+  //   "json",
+  //   "node"
+  // ],
+  // moduleNameMapper: {},
+  // modulePathIgnorePatterns: [],
+  // notify: false,
+  // preset: undefined,
+  // resetMocks: false,
+  // resetModules: false,
+  // resolver: undefined,
+  // restoreMocks: false,
+  // setupFiles: [],
+  // setupFilesAfterEnv: [],
+  // slowTestThreshold: 5,
+  // snapshotSerializers: [],
+  testEnvironment: "jsdom",
+  // testEnvironmentOptions: {},
+  // testLocationInResults: false,
+  // testMatch: [
+  //   "**/__tests__/**/*.[jt]s?(x)",
+  //   "**/?(*.)+(spec|test).[tj]s?(x)"
+  // ],
+  // testPathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
+  // testRegex: [],
+  // testResultsProcessor: undefined,
+  // transform: undefined,
+  // transformIgnorePatterns: [
+  //   "/node_modules/",
+  //   "\\.pnp\\.[^\\/]+$"
+  // ],
+  // unmockedModulePathPatterns: undefined,
+  verbose: true,
+  // watchPathIgnorePatterns: [],
+  // watchman: true,
+};
+
+export default config;
