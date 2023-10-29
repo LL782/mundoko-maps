7939ce90a7da4b53a0a8d259579f4239e9797ed5
Laurence Lord <laurence.lord@ledger.fr>
2023-10-29 23:06:36 +0000

    Use loop functionality for all margin points

--- Mundoko-Maps/src/MapViewer/MapTileSubLayout/ScaleMargins/getScaleMarginsFrom.ts
+++ Mundoko-Maps/src/MapViewer/MapTileSubLayout/ScaleMargins/getScaleMarginsFrom.ts
@@ -1,44 +1,39 @@
 import { PagePosition } from "../../../types/PagePosition";
 import { cityScalePointFrom } from "../../../MapUnits/cityScalePointFrom";
 
-export const getScaleMarginsFrom = (pagePosition: PagePosition) => {
-  const centreOnNorthSouthAxis = cityScalePointFrom(pagePosition.south).value;
-  const centreOnWestEastAxis = cityScalePointFrom(pagePosition.east).value;
+export const getScaleMarginsFrom = (pagePosition: PagePosition) => ({
+  northSouth: {
+    points: [
+      fourDigitStringFrom(pagePosition.south - 50_000),
+      fourDigitStringFrom(pagePosition.south - 40_000),
+      fourDigitStringFrom(pagePosition.south - 30_000),
+      fourDigitStringFrom(pagePosition.south - 20_000),
+      fourDigitStringFrom(pagePosition.south - 10_000),
+      fourDigitStringFrom(pagePosition.south),
+      fourDigitStringFrom(pagePosition.south + 10_000),
+      fourDigitStringFrom(pagePosition.south + 20_000),
+      fourDigitStringFrom(pagePosition.south + 30_000),
+      fourDigitStringFrom(pagePosition.south + 40_000),
+      fourDigitStringFrom(pagePosition.south + 50_000),
+    ],
+  },
+  westEast: {
+    points: [
+      fourDigitStringFrom(pagePosition.east - 50_000),
+      fourDigitStringFrom(pagePosition.east - 40_000),
+      fourDigitStringFrom(pagePosition.east - 30_000),
+      fourDigitStringFrom(pagePosition.east - 20_000),
+      fourDigitStringFrom(pagePosition.east - 10_000),
+      fourDigitStringFrom(pagePosition.east),
+      fourDigitStringFrom(pagePosition.east + 10_000),
+      fourDigitStringFrom(pagePosition.east + 20_000),
+      fourDigitStringFrom(pagePosition.east + 30_000),
+      fourDigitStringFrom(pagePosition.east + 40_000),
+      fourDigitStringFrom(pagePosition.east + 50_000),
+    ],
+  },
+});
 
-  return {
-    northSouth: {
-      points: [
-        fourDigitStringFrom(centreOnNorthSouthAxis - 5),
-        fourDigitStringFrom(centreOnNorthSouthAxis - 4),
-        fourDigitStringFrom(centreOnNorthSouthAxis - 3),
-        fourDigitStringFrom(centreOnNorthSouthAxis - 2),
-        fourDigitStringFrom(centreOnNorthSouthAxis - 1),
-        fourDigitStringFrom(centreOnNorthSouthAxis),
-        fourDigitStringFrom(centreOnNorthSouthAxis + 1),
-        fourDigitStringFrom(centreOnNorthSouthAxis + 2),
-        fourDigitStringFrom(centreOnNorthSouthAxis + 3),
-        fourDigitStringFrom(centreOnNorthSouthAxis + 4),
-        fourDigitStringFrom(centreOnNorthSouthAxis + 5),
-      ],
-    },
-    westEast: {
-      points: [
-        fourDigitStringFrom(centreOnWestEastAxis - 5),
-        fourDigitStringFrom(centreOnWestEastAxis - 4),
-        fourDigitStringFrom(centreOnWestEastAxis - 3),
-        fourDigitStringFrom(centreOnWestEastAxis - 2),
-        fourDigitStringFrom(centreOnWestEastAxis - 1),
-        fourDigitStringFrom(centreOnWestEastAxis),
-        fourDigitStringFrom(centreOnWestEastAxis + 1),
-        fourDigitStringFrom(centreOnWestEastAxis + 2),
-        fourDigitStringFrom(centreOnWestEastAxis + 3),
-        fourDigitStringFrom(centreOnWestEastAxis + 4),
-        fourDigitStringFrom(centreOnWestEastAxis + 5),
-      ],
-    },
-  };
-};
-
-function fourDigitStringFrom(number: number) {
-  return number.toString().padStart(4, "0");
+function fourDigitStringFrom(feet: number) {
+  return cityScalePointFrom(feet).value.toString().padStart(4, "0");
 }
