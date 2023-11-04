5f182fbe0a242c1e4b60fc50e660d13d1c435472
Laurence Lord <laurence.lord@ledger.fr>
2023-11-04 00:55:33 +0000

    Start generating missing tiles from multiple images

--- Mundoko-Maps/src/MapViewer/MapTileSubLayout/MapTile/GeneratedTile.tsx
+++ Mundoko-Maps/src/MapViewer/MapTileSubLayout/MapTile/GeneratedTile.tsx
@@ -1,25 +1,97 @@
-import Image from "next/image";
-import emptyTile from "../../../../public/empty-tile.png";
+import Image, { StaticImageData } from "next/image";
+import emptyTile from "../../../../public/empty-tile-grad.png";
 import { PagePosition } from "../../../types/PagePosition";
+import existingImage from "../../../../public/working-tile.webp";
 
 interface Props {
   position: PagePosition;
 }
 
-export const GeneratedTile = ({ position }: Props) => {
-  console.log("position:", position);
+const TilesFromScaleAbove = () => (
+  <Image
+    alt="Trace of layer above"
+    placeholder="blur"
+    priority={true}
+    src={emptyTile}
+    sizes="100vw"
+    style={{
+      display: "block",
+      width: "100%",
+      height: "auto",
+    }}
+  />
+);
+
+interface PropsSmallTile {
+  matchingImg: StaticImageData | null;
+}
+
+const SmallTile = ({ matchingImg }: PropsSmallTile) => (
+  <Image
+    alt="Trace of tile from layer below"
+    placeholder="blur"
+    priority={true}
+    src={matchingImg || emptyTile}
+    sizes="100vw"
+    style={{
+      float: "left",
+      width: "5%",
+      height: "auto",
+    }}
+  />
+);
+
+const TilesFromScaleBelow = ({ position }: { position: PagePosition }) => {
+  let tiles: Array<PropsSmallTile & { key: string }> = [];
+  for (let i = 0; i < 400; i++)
+    tiles.push({ key: "tile-" + i, matchingImg: null });
+
+  if (
+    (position.scale === "State",
+    position.east === 51350000,
+    position.south === 51550000)
+  ) {
+    tiles[189].matchingImg = existingImage;
+  }
+
   return (
-    <Image
-      alt="Main map"
-      placeholder="blur"
-      priority={true}
-      src={emptyTile}
-      sizes="100vw"
-      style={{
-        display: "block",
-        width: "100%",
-        height: "auto",
-      }}
-    />
+    <>
+      {tiles.map((tile) => (
+        <SmallTile key={tile.key} matchingImg={tile.matchingImg} />
+      ))}
+    </>
+  );
+};
+
+interface PropsTileLayer {
+  children: React.ReactNode;
+}
+
+const TileLayer = ({ children }: PropsTileLayer) => (
+  <div
+    style={{
+      background: "red",
+      position: "absolute",
+      opacity: 0.5,
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    }}
+  >
+    {children}
+  </div>
+);
+
+export const GeneratedTile = ({ position }: Props) => {
+  return (
+    <div style={{ position: "relative", paddingTop: "100%", height: 0 }}>
+      <TileLayer>
+        <TilesFromScaleAbove />
+      </TileLayer>
+      <TileLayer>
+        <TilesFromScaleBelow position={position} />
+      </TileLayer>
+    </div>
   );
 };
--- Mundoko-Maps/src/MapViewer/MapTileSubLayout/MapTile/MapTile.test.tsx
+++ Mundoko-Maps/src/MapViewer/MapTileSubLayout/MapTile/MapTile.test.tsx
@@ -2,17 +2,37 @@ import { render, screen } from "@testing-library/react";
 import { MapTile } from "./MapTile";
 import { PagePosition } from "../../../types/PagePosition";
 
-describe("Given a position that matches an existing map tile image", () => {
+const matchingPosition = {
+  scale: "City",
+  east: 51350000,
+  south: 51550000,
+};
+
+describe("Given a position that matches an existing tile image", () => {
   beforeEach(() => {
-    const position = {
-      scale: "City",
-      east: 51350000,
-      south: 51550000,
-    } as PagePosition;
-    render(<MapTile position={position} />);
+    render(<MapTile position={{ ...matchingPosition } as PagePosition} />);
   });
 
   it("renders that image", () => {
     expect(screen.getByRole("img")).toBeInTheDocument();
+  });
+});
+
+describe("Given the position one scale above does not match an existing tile", () => {
+  beforeEach(() => {
+    render(
+      <MapTile
+        position={
+          {
+            ...matchingPosition,
+            scale: "State",
+          } as PagePosition
+        }
+      />
+    );
+  });
+
+  it("the view is generated from a portion of the tile above and one image for each tile below", () => {
+    expect(screen.getAllByRole("img").length).toBe(401);
   });
 });
